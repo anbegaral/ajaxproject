8c1bdf71bfae0f0671a5b0338a94dcf2a53c4fc5
Ana <ana@intoware.com>
2016-10-19 17:08:24 +0100

    Calling different APIs

--- js/script.js
+++ js/script.js
@@ -12,8 +12,42 @@ function loadData() {
     $nytElem.text("");
 
     // load streetview
+    var $street = $('#street').val();
+    var $city = $('#city').val();
+    var $address = $street + ", "+ $city;
+    var height = window.innerHeight;
+    var width = window.innerWidth;
+    var size = width + "x" + height;
 
-    // YOUR CODE GOES HERE!
+    var $streetview = 'https://maps.googleapis.com/maps/api/streetview?location=' + $address + '&key=AIzaSyA7GFm9PELZ7ALhF_XnL7CvWavW7DWc8Rs&size=' + size +'';
+    $body.append('<img class="bgimg" src="' + $streetview + '">');
+
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=46ef0b9b785b44fe896078856cd28a16&q="+$city+"";
+
+    $.getJSON(url, function(data){
+        var items = [];
+        $.each( data.response.docs, function( key, val ) {
+          items.push("<li id='" + data.response.docs[key]._id + "'><a href='"+ data.response.docs[key].web_url +"'>" + data.response.docs[key].headline.main + "</a><p>" + data.response.docs[key].snippet +"</li>" );
+        });
+
+        $nytElem.html(items);
+    })
+    .error(function(){
+        $nytHeaderElem.text('The New York Times articles could not be loaded.');
+    });
+
+    var urlWiki = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + $city +"&format=json&?callback=?wrapResults";
+
+    $.ajax({
+        url: urlWiki,
+        dataType: "jsonp",
+        success: function(response){
+            var wikiResult = response[1];
+            for (var i = 0; i < wikiResult.length; i++) {
+                $wikiElem.append("<li id='" + wikiResult[i] + "'><a href='"+ response[3][i] +"'>" + wikiResult[i] + "</a></li>");
+            }
+        }
+    });
 
     return false;
 };
